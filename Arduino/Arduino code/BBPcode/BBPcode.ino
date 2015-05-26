01ddeebd8edb617294d26016eb7847a28a8099b0
maestroke <formaestroke@gmail.com>
2015-05-26 17:04:45 +0200

    bpp code added

---
+++ Arduino/Arduino code/BBPcode/BBPcode.ino
@@ -0,0 +1,103 @@
+int input = A0;
+int val = 0;
+int M1 = 5;
+int E1 = 4;
+int M2 = 6;
+int E2 = 7;
+int snelheid = 0;
+int trainPos = 0;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(input, INPUT);
+}
+
+void BBPBandLeft(){
+  digitalWrite(E1, LOW);
+  snelheid = 150;
+}
+
+void BBPBandRight(){
+  digitalWrite(E1, HIGH);
+  snelheid = 150;
+}
+
+void BBPBandStart(){
+  Serial.println("check");
+  digitalWrite(M1, snelheid);
+  Serial.println("check2");
+}
+
+void BBPBandStop(){
+  digitalWrite(M1, 0);
+}
+
+void BBPTrainUp(){
+  digitalWrite(E2, LOW);
+  snelheid = 1;
+}
+
+void BBPTrainDown(){
+  digitalWrite(E2, HIGH);
+  snelheid = 1;
+}
+
+void BBPTrainStop(){
+  digitalWrite(M2, 0);
+}
+
+void BBPTrainStart(){
+  Serial.println("start");
+  if(trainPos = 0){
+    trainPos = 1;
+  }else{
+    trainPos = 0;
+  }
+  digitalWrite(M2, snelheid);
+  delay(400);
+  BBPTrainStop();
+}
+
+void loop(){
+  if(Serial.available() > 0){
+    Serial.println("test");
+    char ch = Serial.read();
+    if(ch == 'l'){
+      Serial.println("test2");
+      BBPBandLeft();
+    }else if(ch == 'r'){
+      BBPBandRight();
+    }else if(ch == 'g'){
+      Serial.println("test2");
+      BBPBandStart();
+    }else if(ch == 's'){
+      BBPBandStop();
+    }else if(ch == 'b'){
+      BBPTrainStart();
+    }else if(ch == 'q'){
+      BBPTrainStop();
+    }else if(ch == 'u'){
+      BBPTrainUp();
+    }else if(ch == 'd'){
+      BBPTrainDown();
+    }
+  }
+  if(analogRead(input) > 1000){
+    Serial.println(analogRead(input));
+    delay(1000);
+    BBPBandStop();
+    if(trainPos = 1){
+      BBPTrainUp();
+      Serial.println("up");
+    }else{
+      BBPTrainDown();
+      Serial.println("down");
+    }
+    BBPTrainStart();
+    delay(1000);
+    BBPBandLeft();
+    BBPBandStart();
+    delay(1000);
+    BBPBandStop();
+  }
+}
